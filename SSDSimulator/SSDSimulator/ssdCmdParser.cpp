056a208de9c899edab79aa2dd4e1dde86fc56716
haewon <baehw1107@naver.com>
2025-07-07 20:29:50 +0900

    [feature] Add SSD Read Command with Singleton pattern

--- SSDSimulator/SSDSimulator/ssdCmdParser.cpp
+++ SSDSimulator/SSDSimulator/ssdCmdParser.cpp
@@ -1,10 +1,12 @@
 #include "ssdInterface.h"
 #include "ssdCmdRead.h"
 #include "ssdCmdParser.h"	
-
-
+#include<string>
 
 SsdCmdInterface* SsdCmdParser::getCommand(const std::string& cmd, const std::string& arg1, const std::string& arg2)
 {
-	return new SsdReadCmd{3};
+	SsdReadCmd& readCmd = SsdReadCmd::getInstance();
+	long address = std::stol(arg1);
+	readCmd.setAddress(address);
+	return &SsdReadCmd::getInstance();
 }
