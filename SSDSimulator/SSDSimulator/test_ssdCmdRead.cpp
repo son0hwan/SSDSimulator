ce9bdcfd008331259d888df239d5bff658750b7d
haewon <baehw1107@naver.com>
2025-07-07 21:23:29 +0900

    [refactor] remove magic number and duplication for Read range Error

--- SSDSimulator/SSDSimulator/test_ssdCmdRead.cpp
+++ SSDSimulator/SSDSimulator/test_ssdCmdRead.cpp
@@ -3,24 +3,40 @@
 #include "ssdCmdRead.h"
 #include <fstream>
 #include <sstream>
+#include <string>
+using namespace testing;
 
-TEST(SSDRead, ReadExecutedWithoutError) {
-    long addr = 19;
-    long expectedData = 0x705ff43a;
-
+class ReadTestFixture : public Test {
+public:
     SsdReadCmd& readCmd = SsdReadCmd::getInstance();
-    EXPECT_NO_THROW(readCmd.run());
+    void runReadTest(long address) {
+        readCmd.setAddress(address);
+        readCmd.run();
+    }
+
+protected:
+    static const long VALID_ADDRESS = 19;
+    static const long INVALID_ADDRESS = 100;
+    static const long EXPECTED_DATA = 0x705ff43a;
+    static const std::string OUTPUT_ERROR;
+    static const std::string OUTPUT_FILENAME;
+};
+
+const std::string ReadTestFixture::OUTPUT_ERROR = "ERROR";
+const std::string ReadTestFixture::OUTPUT_FILENAME = "ssd_output.txt";
+
+
+TEST_F(ReadTestFixture, ReadExecutedWithoutError) {
+    EXPECT_NO_THROW(runReadTest(VALID_ADDRESS));
 }
 
-TEST(SSDRead, ReadExecutedWithError) {
-    SsdReadCmd& readCmd = SsdReadCmd::getInstance();
-    readCmd.setAddress(100);
-    readCmd.run();
+TEST_F(ReadTestFixture, ReadExecutedWithError) {
+    runReadTest(INVALID_ADDRESS);
 
-    std::ifstream outFile("ssd_output.txt");
+    std::ifstream outFile(OUTPUT_FILENAME);
     ASSERT_TRUE(outFile.is_open()) << "ssd_output.txt 파일 열기 실패";
 
     std::string fileContent;
     std::getline(outFile, fileContent);
-    EXPECT_EQ(fileContent, "ERROR");
+    EXPECT_EQ(fileContent, OUTPUT_ERROR);
 }
