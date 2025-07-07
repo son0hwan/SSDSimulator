08bdb718e5b7166bb285be8909a46fa371aee7a7
haewon <baehw1107@naver.com>
2025-07-07 22:08:01 +0900

    [feature] read valid data

--- SSDSimulator/SSDSimulator/ssdInterface.h
+++ SSDSimulator/SSDSimulator/ssdInterface.h
@@ -1,11 +1,11 @@
 ////ssdInterface.h
 #pragma once
+#include <cstdint>
 
 class SsdCmdInterface {
 public:
     virtual void run() = 0;
 };
-
 class SsdErrorCmd : public SsdCmdInterface {
 public:
 	SsdErrorCmd(long address) : address(address) {}
@@ -15,4 +15,9 @@ public:
 
 private:
 	const long address;
+};
+
+struct ReadRawData {
+    uint32_t address;
+    uint32_t data;
 };
