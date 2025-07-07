7deb93fcef64f1dbcc55a82ff23a0c94b95c3e14
wonhwipark <wonhwipark02@gmail.com>
2025-07-07 15:44:21 +0900

    [parser][feature] Adding test fixture of ReadCmd

--- SSDSimulator/SSDSimulator/test_SsdCmdParser.cpp
+++ SSDSimulator/SSDSimulator/test_SsdCmdParser.cpp
@@ -3,22 +3,26 @@
 #include "ssdCmdRead.h"
 #include "ssdCmdWrite.h"
 #include "ssdInterface.h"
+#include "ssdCmdParser.h"
+
 using namespace testing;
 
 class SsdCmdParserFixture : public Test {
  public:
+
+private:
 };
 
-TEST(SsdCmdParserFixture, ReadWithValidAddress) {
-  SsdCmdParser cmdParser;
-  SsdCmdInterface* result = cmdParser.parse("READ", "3");
+TEST_F(SsdCmdParserFixture, ReadWithValidAddress) {
+      SsdCmdParser cmdParser;
+      SsdCmdInterface* result = cmdParser.parse("READ", "3");
 
-  EXPECT_TRUE(nullptr != dynamic_cast<SsdReadCmd*>(result));
+      EXPECT_TRUE(nullptr != dynamic_cast<SsdReadCmd*>(result));
 
-  try {
-    SsdReadCmd* convertedCmd = dynamic_cast<SsdReadCmd*>(result);
-    EXPECT_TRUE(3, convertedCmd->getAddress());
-  } catch (std::exception e){
-  
-  }
+      try {
+        SsdReadCmd* convertedCmd = dynamic_cast<SsdReadCmd*>(result);
+        EXPECT_EQ(3, convertedCmd->getAddress());
+      } catch (std::exception& e){
+          FAIL();
+      }
 }
