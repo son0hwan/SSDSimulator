2259a5f2bbc2d16dbc367dcb2b3ca5321068ed8b
haewon <baehw1107@naver.com>
2025-07-07 22:08:01 +0900

    [feature] read data

--- SSDSimulator/SSDSimulator/ssdInterface.h
+++ SSDSimulator/SSDSimulator/ssdInterface.h
@@ -1,7 +1,13 @@
 ////ssdInterface.h
 #pragma once
+#include <cstdint>
 
 class SsdCmdInterface {
 public:
     virtual void run() = 0;
+};
+
+struct ReadRawData {
+    uint32_t address;
+    uint32_t data;
 };
--- SSDSimulator/SSDSimulator/test_ssdCmdRead.cpp
+++ SSDSimulator/SSDSimulator/test_ssdCmdRead.cpp
@@ -14,16 +14,24 @@ public:
         readCmd.run();
     }
 
+    void deleteFileIfExists(const std::string& filename) {
+        if (std::ifstream(filename)) {
+            std::remove(filename.c_str());  // 기존 파일 삭제
+        }
+    }
+
 protected:
     static const long VALID_ADDRESS = 19;
     static const long INVALID_ADDRESS = 100;
     static const long EXPECTED_DATA = 0x705ff43a;
     static const std::string OUTPUT_ERROR;
     static const std::string OUTPUT_FILENAME;
+    static const std::string NAND_FILENAME;
 };
 
 const std::string ReadTestFixture::OUTPUT_ERROR = "ERROR";
 const std::string ReadTestFixture::OUTPUT_FILENAME = "ssd_output.txt";
+const std::string ReadTestFixture::NAND_FILENAME = "ssd_nand.txt";
 
 
 TEST_F(ReadTestFixture, ReadExecutedWithoutError) {
@@ -39,4 +47,9 @@ TEST_F(ReadTestFixture, ReadExecutedWithError) {
     std::string fileContent;
     std::getline(outFile, fileContent);
     EXPECT_EQ(fileContent, OUTPUT_ERROR);
+}
+
+TEST_F(ReadTestFixture, ReadValidData) {
+    EXPECT_NO_THROW(runReadTest(VALID_ADDRESS));
+    EXPECT_EQ(readCmd.getReadData(), EXPECTED_DATA);
 }
