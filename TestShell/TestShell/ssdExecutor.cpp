7e624dddf7fcf46f771ac6cdc63c3c7b4bf06e6c
yeonghwan son <son0hwan@naver.com>
2025-07-07 15:08:28 +0900

    [shell][feature] support mockExecutor

---
+++ TestShell/TestShell/ssdExecutor.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include "executor.h"
+
+class SSDExecutor : public Executor {
+    std::string readFromSSD(int address) override {
+        std::string command = "ssd.exe R " + std::to_string(address);
+        int result = std::system(command.c_str());
+        if (result != 0) {
+            std::cout << "read fail : return code = " << result << std::endl;
+        }
+    }
+
+    std::string writeToSSD(int address, int value) override {
+        char hexStr[10];
+        std::snprintf(hexStr, sizeof(hexStr), "0x%X", value);
+        std::string command = "ssd.exe W " + std::to_string(address) + " " + hexStr;
+        int result = std::system(command.c_str());
+        if (result != 0) {
+            std::cout << "write fail : return code = " << result << std::endl;
+        }
+    }
+};
