d5c8c7c9ea1e56ea8f4d4e2febd1651bd151b190
wonhwipark <wonhwipark02@gmail.com>
2025-07-08 09:51:51 +0900

    [parser][feature] implement shell cmd read
    [parser][feature] Adding testShell parser red
    
    [parser][feature] implement shell cmd read

--- TestShell/TestShell/testShellCmdRead.h
+++ TestShell/TestShell/testShellCmdRead.h
@@ -2,21 +2,22 @@
 #include "testShellCmdInterface.h"
 
 class TestShellReadCmd : public TestShellCmdInterface {
-public:
-	// 생성자는 parser에서 불러줄 것
-	TestShellReadCmd(long address) : address(address) {}
+ public:
+  // 생성자는 parser에서 불러줄 것
+  TestShellReadCmd(long address) : address(address) {}
 
-	void run() override {}
+  void run() override {}
 
-private:
-	const long address;
+  long getAddress() { return address; }
+
+ private:
+  const long address;
 };
 
-
 class TestShellFullReadCmd : public TestShellCmdInterface {
-public:
-	// 생성자는 parser에서 불러줄 것
-	TestShellFullReadCmd() {}
+ public:
+  // 생성자는 parser에서 불러줄 것
+  TestShellFullReadCmd() {}
 
-	void run() override {}
+  void run() override {}
 };
--- TestShell/TestShell/test_ShellCmdParser.cpp
+++ TestShell/TestShell/test_ShellCmdParser.cpp
@@ -1,23 +1,43 @@
 #include <vector>
+
 #include "gmock/gmock.h"
+#include "shellCmdParser.h"
 #include "stdexcept"
+#include "testShellCmdInterface.h"
+#include "testShellCmdRead.h"
+#include "testShellCmdWrite.h"
 
 using namespace testing;
 using namespace std;
 
 class ShellCmdParserFixture : public Test {
  public:
-
-  void SetUp() override {
-    std::streambuf* backup = std::cout.rdbuf();
-    std::cout.set_rdbuf(localStream.rdbuf());
-  }
-
-  void TearDown() override { std::cout.set_rdbuf(backup); }
-
-  string getPrintedString() const { return localStream.str(); }
-
-  std::ostringstream localStream;
-  std::streambuf* backup = nullptr;
-
 };
+
+TEST_F(ShellCmdParserFixture, ReadWithValidAddress) {
+  ShellCmdParser cmdParser;
+  TestShellCmdInterface* command = cmdParser.getCommand({"read", "0"});
+
+  EXPECT_TRUE(nullptr != dynamic_cast<TestShellReadCmd*>(command));
+  try {
+    TestShellReadCmd* convertedCmd =
+        dynamic_cast<TestShellReadCmd*>(command);
+    EXPECT_EQ(0, convertedCmd->getAddress());
+  } catch (std::exception& e) {
+    FAIL();
+  }
+}
+
+TEST_F(ShellCmdParserFixture, ReadWithInValidCommand) {
+  ShellCmdParser cmdParser;
+  TestShellCmdInterface* command = cmdParser.getCommand({"READ", "3"});
+
+  EXPECT_TRUE(nullptr != dynamic_cast<TestShellErrorCmd*>(command));
+}
+
+TEST_F(ShellCmdParserFixture, ReadWithInValidAddress) {
+  ShellCmdParser cmdParser;
+  TestShellCmdInterface* command = cmdParser.getCommand({"read", "AAA"});
+
+  EXPECT_TRUE(nullptr != dynamic_cast<TestShellErrorCmd*>(command));
+}
