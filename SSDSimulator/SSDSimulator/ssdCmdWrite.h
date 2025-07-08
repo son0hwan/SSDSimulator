24ed84288bd54626d133b7ac62c464907dfa0922
JH <emlapl@naver.com>
2025-07-08 15:11:54 +0900

    [refactoring] removed redundant code in ssdWriteCmd class

--- SSDSimulator/SSDSimulator/ssdCmdWrite.h
+++ SSDSimulator/SSDSimulator/ssdCmdWrite.h
@@ -19,15 +19,9 @@ public:
 
     void run() override;
     void setAddress(uint32_t newAddress);
-    void setWriteData(uint32_t newWriteData);
-    void readNandData(const std::string& filename);
-    void updateOutputWriteSuccess();
-    void updateOutputError();
-    void updateNandData();
-    void WriteSectorAddressAndDataToNAND(std::ofstream& nandDataFile, uint32_t addr);
-    uint32_t getAddress() { return requestedAddress; }
-    uint32_t getValue() { return data;  }
-    uint32_t getData() const { return data; }
+    void setWriteData(uint32_t newWriteData);   
+    uint32_t getAddress();
+    uint32_t getData() const;
 
 private:
     SsdWriteCmd() : requestedAddress() {}
@@ -36,16 +30,4 @@ private:
 
     uint32_t requestedAddress;
     uint32_t data;
-    static const int DEVICE_MAX_ADDRESS = 99;
-
-    const std::string NAND_DATA_FILE = "ssd_nand.txt";
-    const std::string OUTPUT_FILE = "ssd_output.txt";
-    const std::string SEPARATOR = ";";
-    const std::string OUTPUT_ERROR = "ERROR";
-
-    bool CheckAddressRange(uint32_t address);
-    void loadDataFromNand(const std::string& filename);
-    void updateDataInInternalBuffer(uint32_t address, uint32_t data);
-
-    std::vector<ReadRawData> readRawData;
 };
