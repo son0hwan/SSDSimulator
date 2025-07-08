5a325785412d76a7cfbd0478c76e9d7326e412f8
BeomsuKim <beomsu7.kim@samsung.com>
2025-07-08 09:06:21 +0900

    [shell][refactor][ver2] tc : read & fullread

--- TestShell/TestShell/testShell.cpp
+++ TestShell/TestShell/testShell.cpp
@@ -1,10 +1,25 @@
 #include "executor.h"
+
+#define MAX_LBA 100
+
 class TestShell {
 public:
 	TestShell(Executor* executor) : executor(executor) {}
 	int write(int address, int value) {
 		executor->writeToSSD(address, value);
 		return 0;
+	}
+
+	std::string read(int address) {
+		return executor->readFromSSD(address);
+	}
+
+	bool fullRead() {
+		for (int sector = 0; sector < MAX_LBA; sector++) {
+			if (executor->readFromSSD(sector) == "ERROR")
+				return false;
+		}
+		return true;
 	}
 	
 private:
---
+++ TestShell/TestShell/test_read.cpp
@@ -0,0 +1,49 @@
+#include <random>
+#include "mockExecutor.cpp"
+#include "testShell.cpp"
+
+#define FULL_READ_COUNT 100
+#define MAX_STR_LENGTH 8
+#define MAX_ALPHABET_SIZE 26
+#define RESULT_STR_SUCCESS ""
+
+std::vector<std::string> generateAlphabet() {
+	std::vector<std::string> result;
+	for (char c = 'A'; c <= 'Z'; ++c) {
+		result.emplace_back(1, c);
+	}
+	return result;
+}
+
+void setRandomStrInHex(std::string& EXPECTED_STR) {
+	std::vector<std::string> alphabetList = generateAlphabet();
+
+	EXPECTED_STR = "0x";
+	for (int i = 0; i < MAX_STR_LENGTH; i++)
+		EXPECTED_STR.append(alphabetList[rand() % MAX_ALPHABET_SIZE]);
+
+}
+
+TEST(TS_READ, READ_JUST_ONCE) {
+	MockSSD mockSSD;
+	TestShell testShell{ &mockSSD };
+
+	std::string EXPECTED_STR;
+	setRandomStrInHex(EXPECTED_STR);
+	
+	EXPECT_CALL(mockSSD, readFromSSD)
+		.WillOnce(testing::Return(EXPECTED_STR));
+
+	EXPECT_EQ(EXPECTED_STR, testShell.read(50));
+}
+
+TEST(TS_READ, READ_FULL) {
+	MockSSD mockSSD;
+	TestShell testShell{ &mockSSD };
+	for (int i = 0; i < FULL_READ_COUNT; i++) {
+		EXPECT_CALL(mockSSD, readFromSSD(i))
+			.WillOnce(testing::Return(RESULT_STR_SUCCESS));
+	}
+
+	testShell.fullRead();
+}
