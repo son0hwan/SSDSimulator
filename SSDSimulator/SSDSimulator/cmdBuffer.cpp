a5a141f9b82095dedc34aa18ca650d9ee765b617
sj21.lim <lsj960427@gmail.com>
2025-07-09 11:25:16 +0900

    [cmdBuffer] implement cbuffer

---
+++ SSDSimulator/SSDSimulator/cmdBuffer.cpp
@@ -0,0 +1,28 @@
+#include "cmdBuffer.h"
+
+CmdQ_type CommandBuffer::addBufferAndGetCmdToRun(SsdCmdInterface* newCmd) {
+  CmdQ_type resultQ;
+  if (false == newCmd->isAllowBuffering()) {
+    resultQ.push_back(newCmd);
+    return resultQ;
+  }
+
+  cmdQ.push_back(newCmd);
+
+  if (cmdQ.size() > Q_SIZE_LIMIT_TO_FLUSH) {
+    resultQ = popAllBuffer();
+  }
+
+  //storage.setBufferToStorage(cmdQ);
+  return resultQ;
+}
+
+CmdQ_type CommandBuffer::popAllBuffer() {
+  CmdQ_type result = std::move(cmdQ);
+  cmdQ = CmdQ_type{};
+  return result;
+}
+
+CmdQ_type CommandBufferStroage::getBufferFromStorage() { return CmdQ_type{}; }
+
+void CommandBufferStroage::setBufferToStorage(CmdQ_type cmdQ) {}
---
+++ SSDSimulator/SSDSimulator/cmdBuffer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <deque>
+#include "ssdInterface.h"
+
+using std::deque;
+typedef deque<SsdCmdInterface*> CmdQ_type;
+
+class CommandBufferStroage {
+public:
+	virtual CmdQ_type getBufferFromStorage();
+	virtual void setBufferToStorage(CmdQ_type cmdQ);
+};
+
+class CommandBuffer {
+public:
+	static CommandBuffer& getInstance() {
+		static CommandBufferStroage storage{};
+		static CommandBuffer instance{ storage };
+		return instance;
+	}
+
+	CommandBuffer(CommandBufferStroage& stroage)
+		: storage(storage) {}
+
+	CmdQ_type addBufferAndGetCmdToRun(SsdCmdInterface* newCmd);
+	CmdQ_type popAllBuffer();
+
+
+protected:
+	CmdQ_type cmdQ;
+	CommandBufferStroage& storage;
+
+	static const int Q_SIZE_LIMIT_TO_FLUSH = 5;
+};
+
