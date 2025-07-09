3e1610614ac191d401fb8c54cd331eb864bf4f78
sj21.lim <lsj960427@gmail.com>
2025-07-09 13:02:15 +0900

    [cmdBuffer][feature] add cmdBuffer to main

--- SSDSimulator/SSDSimulator/cmdBuffer.cpp
+++ SSDSimulator/SSDSimulator/cmdBuffer.cpp
@@ -1,31 +1,36 @@
 #include "cmdBuffer.h"
 
+CommandBuffer::CommandBuffer(CommandBufferStroage& stroage)
+	: storage(storage) {
+	cmdQ = storage.getBufferFromStorage();
+}
+
 CmdQ_type CommandBuffer::addBufferAndGetCmdToRun(SsdCmdInterface* newCmd) {
-  CmdQ_type resultQ;
-  if (false == newCmd->isAllowBuffering()) {
-   //TBD : if buffer read and cached,  ssdBufferReadCmd(value);
+	CmdQ_type resultQ;
+	if (false == newCmd->isAllowBuffering()) {
+		//TBD : if buffer read and cached,  ssdBufferReadCmd(value);
 
 
-    resultQ.push_back(newCmd);
-    return resultQ;
-  }
+		resultQ.push_back(newCmd);
+		return resultQ;
+	}
 
-  if (cmdQ.size() >= Q_SIZE_LIMIT_TO_FLUSH) {
-      resultQ = popAllBuffer();
-  }
+	if (cmdQ.size() >= Q_SIZE_LIMIT_TO_FLUSH) {
+		resultQ = popAllBuffer();
+	}
 
-  cmdQ.push_back(newCmd);
+	cmdQ.push_back(newCmd);
 
 
 
-  //storage.setBufferToStorage(cmdQ);
-  return resultQ;
+	//storage.setBufferToStorage(cmdQ);
+	return resultQ;
 }
 
 CmdQ_type CommandBuffer::popAllBuffer() {
-  CmdQ_type result = std::move(cmdQ);
-  cmdQ = CmdQ_type{};
-  return result;
+	CmdQ_type result = std::move(cmdQ);
+	cmdQ = CmdQ_type{};
+	return result;
 }
 
 CmdQ_type CommandBufferStroage::getBufferFromStorage() { return CmdQ_type{}; }
